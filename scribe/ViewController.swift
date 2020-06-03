f0791a86500570e20976448bf70f8cc3007cf059
Victor Yamchinov <kodiers@gmail.com>
2020-06-04 01:03:55 +0300

    Initial Commit

---
+++ scribe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scribe
+//
+//  Created by Viktor Yamchinov on 04.06.2020.
+//  Copyright © 2020 Viktor Yamchinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
